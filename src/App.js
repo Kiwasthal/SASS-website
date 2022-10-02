3ac75be4f3851bcf3589e7622993e8da6eeba1c9
Steerpike <imthekoul@gmail.com>
2022-10-02 17:41:46 +0300

    Mixins -- theming && query screens

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
+import MainSection from './Components/Boot/MainSeciton';
 import './scss/main.scss';
 
 function App() {
   return (
-    <div className="App">
-      <main className="main">
-        <p className="main-paragraph">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </main>
+    <div className="app light">
+      <MainSection />
     </div>
   );
 }
