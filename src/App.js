e6b098c8cac86c8e9ca9f23a3476458d4aa9d3b5
Steerpike <imthekoul@gmail.com>
2022-10-02 20:51:19 +0300

    Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import MainSection from './Components/Boot/MainSeciton';
+import Header from './Components/Project/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Project from './pages/Project';
+import Contact from './pages/Contact';
+
 import './scss/main.scss';
 
 function App() {
   return (
-    <div className="app light">
-      <MainSection />
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/projects" element={<Project />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
+    </>
   );
 }
 
