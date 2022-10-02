f026d06a69499c2d254906d680ecccc2db2c880b
Steerpike <imthekoul@gmail.com>
2022-10-02 14:46:09 +0300

    Sass Nesting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import MainPage from './Components/MainPage';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <MainPage />
     </div>
   );
 }
